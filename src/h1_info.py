e747e072dd98c903b2fa89919454075f6c8324fd
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2021-12-13 09:36:33 +0000

    Added regex for hostnames to strip non-alpha characters

--- src/h1_info.py
+++ src/h1_info.py
@@ -2,6 +2,9 @@ import requests
 from conf.config import H1Program, filename, api_get_programs_endpoint
 import csv
 from conf.base_logger import logger
+import re
+
+hostname_regex = re.compile("[^a-zA-Z]", flags=re.A)
 
 
 def write_results_to_file(results: list):
@@ -17,6 +20,8 @@ def write_results_to_file(results: list):
         csv_out = csv.writer(result_file)
         csv_out.writerow(['id', 'name', 'offers_bounties', 'triage_active'])
         for r in results:
+            if r.name is None:
+                logger.debug(f"Didn't write ")
             csv_out.writerow(r)
     return True
 
@@ -35,9 +40,9 @@ def send_request(username, token):
 
 def get_h1_programs(username, token):
     """
-
-    :param username:
-    :param token:
+    When checking for a valid hostname, code ALWAYS return a hostname to keep downstream code cleaner
+    :param username: H1 username
+    :param token: H1 API token ( Hacker Token not Company Token )
     :return: List
     """
     resp = send_request(username, token)
@@ -51,8 +56,8 @@ def get_h1_programs(username, token):
     #     logging.info(f'[*]H1 Programs found {len(h1_programs)}.\tPagination found: {next_url}')
     #     get_h1_programs(username, token, next_url, h1_programs)
 
-    # at this point, you unwind the recursion. Iterating through all H1 Program with a for loop
+    # Unwind the recursion. Iterating through all H1 Program with a for loop
     return [H1Program(p.get('id'),
-                      p.get('attributes').get('name'),
+                      re.sub(hostname_regex, '', p.get('attributes').get('name')).lower(),
                       p.get('attributes').get('triage_active'),
                       p.get('attributes').get('offers_bounties')) for p in h1_results]
